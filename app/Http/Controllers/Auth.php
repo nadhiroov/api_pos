7e191f66ced3165b100dd2cef42ea174b197282e
nadhiroov <nadhirov@gmail.com>
2025-04-08 12:06:29 +0700

    first commit

---
+++ app/Http/Controllers/Auth.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class Auth extends Controller
+{
+    public function register(RegisterRequest $request) : JsonResponse
+    {
+        $data = $request->validated();
+        if (User::where('username', $data['username'])->count() == 1) {
+            throw new HttpResponseException(response([
+                "errors" => [
+                    "username" => [
+                        "username already registered"
+                    ]
+                ]
+            ], 400));
+        }
+        $user = new User($data);
+        $user->password = Hash::make($data['password']);
+        $user->save();
+        return response()->json([
+            "message" => "User created successfully"
+        ], 201);
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
