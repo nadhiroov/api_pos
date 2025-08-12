0a4096a72c440ba22850f89afe6506628035994a
Nadhir Barabud <nadhirov@gmail.com>
2025-08-12 23:57:04 +0700

    ✨ update logo images in header and sidebar for branding consistency

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,8 +13,8 @@
             </ul>
             <div class="d-block d-lg-none py-4">
                 <a href="../main/index.html" class="text-nowrap logo-img">
-                    <img src="{{ asset('assets/images/logos/dark-logo.svg') }}" class="dark-logo" alt="Logo-Dark" />
-                    <img src="{{ asset('assets/images/logos/dark-logo.svg') }}" class="light-logo" alt="Logo-light" />
+                    <img src="{{ asset('assets/images/logos/rahat-dark.svg') }}" class="dark-logo" alt="Logo-Dark" />
+                    <img src="{{ asset('assets/images/logos/rahat-light.svg') }}" class="light-logo" alt="Logo-light" />
                 </a>
             </div>
             <a class="navbar-toggler nav-icon-hover-bg rounded-circle p-0 mx-0 border-0" href="javascript:void(0)"
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,9 +3,9 @@
         <!-- Start Vertical Layout Sidebar -->
         <!-- ---------------------------------- -->
         <div class="brand-logo d-flex align-items-center justify-content-between">
-            <a href="../main/index.html" class="text-nowrap logo-img">
-                <img src="{{ asset('assets/images/logos/dark-logo.svg') }}" class="dark-logo" alt="Logo-Dark" />
-                <img src="{{ asset('assets/images/logos/dark-logo.svg') }}" class="light-logo" alt="Logo-light" />
+            <a href="/dashboard" class="text-nowrap logo-img">
+                <img src="{{ asset('assets/images/logos/rahat-dark.svg') }}" class="dark-logo" alt="Logo-Dark" />
+                <img src="{{ asset('assets/images/logos/rahat-light.svg') }}" class="light-logo" alt="Logo-light" />
             </a>
             <a href="javascript:void(0)" class="sidebartoggler ms-auto text-decoration-none fs-5 d-block d-xl-none">
                 <i class="ti ti-x"></i>
